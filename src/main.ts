328140ec98339aac9af42ba8dc1f4aeee67ae6d4
Suzan Rana <dev.suzanrana@gmail.com>
2023-09-24 17:19:14 +0545

    Feat: NestJs logger added!

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      skipMissingProperties: true,
+    }),
+  );
+
+  const [port, NODE_ENV] = [3000, 'dev'];
+  await app.listen(port);
+
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port} Environment: ${NODE_ENV}`,
+  );
 }
+
 bootstrap();
